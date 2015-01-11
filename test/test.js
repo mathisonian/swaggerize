a650702d9918c71aef04755292f1a4189deedd22
Matthew Conlen <mc@mathisonian.com>
2015-01-11 16:24:55 -0500

    update test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,19 @@
 'use strict';
 
+var expect = require('expect.js');
+
+
 describe('Swaggerize', function() {
     describe('Generate API', function() {
         it('should generate a Swagger API', function(done) {
             var db = require('../fixtures/models/index.js')({});
             var spec = db.swaggerize();
-            console.log(spec);
+
+            var expected = require('../sample/fixture.json');
+
+            spec = JSON.parse(spec);
+
+            expect(spec).to.eql(expected);
             done();
         });
     });
