adda62ddddc4ddfbfb7fbb4adcd77d0b9639c1a5
sramam <sramam@gmail.com>
2015-01-11 06:55:37 -0800

    fix tests to work

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 describe('Swaggerize', function() {
     describe('Generate API', function() {
         it('should generate a Swagger API', function(done) {
-            var db = require('../fixtures/db.js')({});
+            var db = require('../fixtures/models/index.js')({});
             var spec = db.swaggerize();
             console.log(spec);
             done();
